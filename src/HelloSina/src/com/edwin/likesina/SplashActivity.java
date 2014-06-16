7e2c20240944b4c9b8c43fa342df05f6a9231bd9
Edwin Li <helloedwinli@gmail.com>
2014-06-17 00:01:57 +0800

    Add project

---
+++ src/HelloSina/src/com/edwin/likesina/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.edwin.likesina;
+
+import com.edwin.likesina.home.activity.HomeMenuActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, HomeMenuActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
