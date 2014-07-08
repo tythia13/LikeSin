c56336361cb27bf461c823e143f6e9c1ac6a66ec
Edwin Li <helloedwinli@gmail.com>
2014-07-08 23:47:43 +0800

    Add gitignore

--- src/LikeSin/src/com/edwin/likesina/SplashActivity.java
+++ src/LikeSin/src/com/edwin/likesina/SplashActivity.java
@@ -1,10 +1,11 @@
 package com.edwin.likesina;
 
-import com.edwin.likesina.app.home.activity.HomeMenuActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.edwin.likesina.app.home.activity.HomeMenuActivity;
 
 public class SplashActivity extends Activity
 {
@@ -13,6 +14,13 @@ public class SplashActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         // Intent intent = new Intent(this, MainActivity.class);
+        String a = "test";
+        Log.e("edwin", "a="+a);
+        a = "treat";
+        Log.e("edwin", "a="+a);
+        String b = a.substring(0,2);
+        Log.e("edwin", "b="+b);
+        
         Intent intent = new Intent(this, HomeMenuActivity.class);
         startActivity(intent);
         finish();
